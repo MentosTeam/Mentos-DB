76f470ba453eea499195126941b0879cad8cce0c
kimtaehyun98 <kimtaehyun98@naver.com>
2022-01-23 18:46:15 +0900

    멘토쓰 찾기 글 Image도 같이 반환하는 테스트 쿼리문 추가

--- testData.sql
+++ testData.sql
@@ -10,6 +10,10 @@ INSERT INTO member VALUES ( 2, '멘티', '멘티', '17', 'menti@naver.com', '123
 INSERT INTO mento VALUES (1, 1, 2, 3, '', 5.0, '안녕하세요!', current_timestamp, current_timestamp);
 INSERT INTO menti VALUES (2, 1, 2, 3, '', '멘티입니다', current_timestamp, current_timestamp);
 
+INSERT INTO image VALUES (1, 1, 'imageUrl_1-1');
+INSERT INTO image VALUES (2, 1, 'imageUrl_1-2');
+INSERT INTO image VALUES (3, 2, 'imageUrl_2-1');
+
 INSERT INTO post VALUES (1, 1, 1, 'Spring Framework를 사용한 웹 프로그래밍(WEB)', 'Spring을 사용하여 Web BackEnd를 공부해왔습니다. 전반적인 서버 관련된 멘토링이 가능합니다. #Spring #JPA #MySQL #졸려',
                          current_timestamp, current_timestamp);
 
